5b7a84983a313794f59ef3265001c08f67d33941
webcrawls <github.com@kaden.sh>
2023-07-14 04:57:00 -0400

    ✨ debounce parsing

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,12 @@
 export * from './parser'
 export * from './stores/local'
+
+export const debounce = (func: (value: string) => void, wait = 250) => {
+    let debounceTimeout: number;
+    return (value: string) => {
+        clearInterval(debounceTimeout);
+        debounceTimeout = window.setTimeout(() => {
+            func(value);
+        }, wait);
+    };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,17 @@
     import {browser} from "$app/environment";
     import {localStore} from "$lib/stores/local";
     import {writable} from "svelte/store";
+    import {debounce} from "$lib";
 
     const content = browser ? localStore("gmi-viewer:content", {text: "Hello!"}) : writable({text: "Hello!"})
+    const handleUpdate = debounce((e) => $content.text = e.detail, 100)
 </script>
 
 <main>
     <section>
         <header>Input</header>
         <div class="section-content">
-            <DocumentInput content="{$content.text}" on:update={(e) => $content.text = e.detail}/>
+            <DocumentInput content="{$content.text}" on:update={handleUpdate}/>
         </div>
     </section>
     <section>
